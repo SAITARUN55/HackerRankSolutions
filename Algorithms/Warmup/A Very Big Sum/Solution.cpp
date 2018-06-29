d9c65c4f313db9ff9eb66a55e12a7b0795c05045
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2018-06-29 11:48:35 +0530

    Create Solution.cpp

---
+++ Algorithms/Warmup/A Very Big Sum/Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    long long int n,sum=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        long long int a;
+    cin>>a;
+    sum=sum+a;
+    }
+    cout<<sum<<endl;
+    return 0;
+    
+}
